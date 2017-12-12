774bc7664c87dfdabafa81c09d9e5625fd5cfc23
Noah Fernandes <>
2017-12-12 11:02:54 +0100

    Created a temp keys file

---
+++ keys/index_temp.js
@@ -0,0 +1,9 @@
+module.exports = {
+    GOOGLE_API_KEY: '',
+    GOOGLE_CLIENT_ID: '',
+    GOOGLE_CLIENT_SECRET: '',
+    FACEBOOK_APP_ID: '',
+    FACEBOOK_APP_SECRET: '',
+    MONGO_URI: '',
+    COOKIE_KEY: ''
+};
